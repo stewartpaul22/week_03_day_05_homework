a9ce91057b84d53123e6386b58d67ad994e8e679
Paul Stewart <stewartpaul22@yahoo.co.uk>
2018-02-09 21:13:37 +0000

    Add a save() method to the Ticket class

--- models/ticket.rb
+++ models/ticket.rb
@@ -8,4 +8,23 @@ class Ticket
     @movie_id = options['movie_id'].to_i
   end
 
+  def save()
+    sql = "INSERT INTO tickets (customer_id, movie_id) VALUES ($1, $2) RETURNING id"
+    values = [@customer_id, @movie_id]
+    ticket = SqlRunner.run(sql, values).first
+    @id = tickets['id'].to_i
+  end
+
+  def edit()
+  end
+
+  def self.all()
+  end
+
+  def delete()
+  end
+
+  def self.delete_all()
+  end
+
 end
