df37686ce47d17616f5d68e742fc0c868c55b999
Paul Stewart <stewartpaul22@yahoo.co.uk>
2018-02-09 20:40:55 +0000

    Add an all() method to the Movie class

--- models/movie.rb
+++ models/movie.rb
@@ -2,6 +2,8 @@ require_relative('../db/sql_runner.rb')
 
 class Movie
 
+  attr_accessor :title
+
   def initialize(options)
     @id = options['id'].to_i if options['id']
     @title = options['title']
@@ -19,6 +21,13 @@ class Movie
     sql = "UPDATE movies SET (title, price) = ($1, $2) WHERE id=$3"
     values = [@title, @price, @id]
     SqlRunner.run(sql, values)
+  end
+
+  def self.all()
+    sql = "SELECT * FROM movies"
+    values = []
+    movies = SqlRunner.run(sql, values)
+    return movies.map{|customer| Customer.new(customer)}
   end
 
   
