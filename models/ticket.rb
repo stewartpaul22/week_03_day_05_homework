54446d76cfd00c8e647390f2eeb233b2e97bb3fc
Paul Stewart <stewartpaul22@yahoo.co.uk>
2018-02-09 21:07:01 +0000

    Add an initialize() method to the Ticket class

--- models/ticket.rb
+++ models/ticket.rb
@@ -1,1 +1,11 @@
 require_relative('../db/sql_runner.rb')
+
+class Ticket
+
+  def initialize(options)
+    @id = options['id'].to_i if options['id']
+    @customer_id = options['customer_id'].to_i
+    @movie_id = options['movie_id'].to_i
+  end
+
+end
